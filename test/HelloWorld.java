6ed0ad0f2f7715a0dc9ba2c0ff3853162e32c8ea
Sushil Mishra <sushilmishra9699@gmail.com>
2023-03-06 13:34:47 +0530

    Adding Test dir to the repo

---
+++ test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mindtree.utility;
+
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hell0o WOrld!");
+        // Adding oneclearmore print ststemanet
+        System.out.println("adding this print statement");
+        System.out.println("adding this print statement again");
+
+    }
+}
